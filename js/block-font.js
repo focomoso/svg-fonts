db997f72cfad39ac2436809ffc48b6c84cb85175
focomoso <jkelly@focomoso.com>
2015-02-20 10:41:44 -0800

    initial commit

---
+++ js/block-font.js
@@ -0,0 +1,74 @@
+/**
+ * Static class holdig the source arrays for the Block Font
+ * @type {BlockSrc}
+ */
+var BlockSrc = {
+  "a":  [
+          [0,1,1,1,0],
+          [1,0,0,0,1],
+          [1,0,0,0,1],
+          [1,0,0,0,1],
+          [1,1,1,1,1],
+          [1,0,0,0,1],
+          [1,0,0,0,1]
+        ],
+  "b": [
+          [1,1,1,1,0],
+          [1,0,0,0,1],
+          [1,0,0,0,1],
+          [1,1,1,1,0],
+          [1,0,0,0,1],
+          [1,0,0,0,1],
+          [1,1,1,1,0]
+        ]
+};
+
+/**
+ * Generates voxel based characters as svgs
+ */
+var BlockFont = (function() {
+
+  var _this = this;
+
+  /**
+   * The width in px of each block
+   * @type {Number}
+   */
+  this.blockW = 10;
+  this.blockH = 10;
+  this.color = "#808080";
+
+  /**
+   * Init
+   * @param {number} bw Width of each block
+   * @param {number} bh Height of each block
+   * @param {string} bc Hex string for color
+   */
+  function BlockFont(bw, bh, bc) {
+
+    if (typeof bw === 'undefined')
+      _this.blockW = bw;
+
+    if (typeof bh === 'undefined')
+      _this.blockH = bh;
+
+    if (typeof bc === 'undefined')
+      _this.blockColor = bc;
+
+  }
+
+  /////////////////////////
+  // getters and setters //
+  /////////////////////////
+
+  BlockFont.prototype.getColor = function() {
+    return _this.color;
+  };
+
+  BlockFont.prototype.setColor = function(color) {
+    _this.color = color;
+  };
+
+  return BlockFont;
+
+})();
