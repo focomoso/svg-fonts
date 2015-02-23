da36768f62a105ff00858cc96d9540c3e137e1ad
focomoso <jkelly@focomoso.com>
2015-02-22 21:26:09 -0800

    blockFont functions return objects now so we can keep runnig track of the width of the svgs

--- js/block-font.js
+++ js/block-font.js
@@ -35,33 +35,55 @@ var BlockFont = (function () {
        * @param  {Char}       glyph   The character to be drawn
        * @param  {HexString}  color   Hex string for the highlight color to draw
        * @param  {String}     dir     The orientation of the character 'x'=\ 'y'=/ 'zx'= facing |, top \ zy'= facing |, top /
-       * @return {String}             An svg of the character
+       * @return {Object}             An svg of the character
        */
       writeGlyph: function(glyph, color, dir, x, y) {
-        var str = '<rect x="' + x + '" y="' + y + '" width="80" height="100" fill="red"/>';
-        return str;
+        var width = 80;
+        var str = '<rect x="' + x + '" y="' + y + '" width="' + width + '" height="100" fill="#' + color + '"/>';
+        return {
+          svgStr: str,
+          width: width
+        };
       },
 
       /**
-       * Returns an svg of the given string with the given color
-       * @param  {String} str       The string to write
-       * @param  {String} color     Hex string for the highlight color
-       * @param  {String} charDir   The orientation of the character 'x'=\ 'y'=/ 'zx'= facing |, top \ zy'= facing |, top /
-       * @param  {String} stringDir The direction of the entire string 'x'= \; 'y'= /; 'z'= |; 'h'= -;
-       * note: charDir y will usually want to go in stringDir x
-       * @return {String}           An svg of the scring
+       * Writes an svg into the given selector with the given string with the given color
+       * @param  {String} selector    The jQuery selector to write the string into
+       * @param  {String} str         The string to write
+       * @param  {String} color       Hex color of the highlight
+       * @param  {String} charDir     The orientation of the character 'x'=\ 'y'=/ 'zx'= facing |, top \ zy'= facing |, top /
+       * @param  {String} stringDir   The direction of the entire string 'x'= \; 'y'= /; 'z'= |; 'h'= -;
+       *                                note: charDir y will usually want to go in stringDir x
+       * @param  {Number} charHeight  The preffered height of each character
+       * @return {Object}             The svg string, width and height of the final svg.
        */
-      writeString: function(str, color, charDir, stringDir) {
-        svgStr = '<svg width="600" height="120">';
+      writeString: function(selector, str, color, charHeight, charDir, stringDir) {
+
+        var x = y = 0;
+        var width = height = 0;
+        var bodyStr = '';
+
+        $(selector).empty();
 
         var strData = str.split('');
 
         $.each(strData, function(i,v){
-          svgStr += instance.writeGlyph(v, color, charDir, i*100, 0);
+          glyph = instance.writeGlyph(v, color, charDir, i*100, 0);
+          bodyStr += glyph.svgStr;
+          width += glyph.width;
         });
-        svgStr += "</svg>";
 
-        return svgStr;
+        svgStr = '<svg width="' + width + '" height="120" overflow="visible">'
+            + bodyStr
+            + "</svg>";
+
+        $(selector).append(svgStr);
+
+        return {
+          str: svgStr,
+          width: width,
+          height: height
+        };
       }
     };
   };
